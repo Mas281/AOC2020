1ce9e179a2786ea6c1b3a826a311b4f067b77874
Mas281 <github@samdev.io>
2021-01-23 22:49:41 +0000

    Regexify day 19 and add part 2

--- day19/day19.py
+++ day19/day19.py
@@ -1,3 +1,6 @@
+import functools
+import re
+
 with open("input.txt") as file:
     lines = file.read().splitlines()
 
@@ -10,7 +13,7 @@ def parse_rules():
 
         if len(split) == 2:
             if split[1].isdigit():
-                rules[number] = int(split[1])
+                rules[number] = [[int(split[1])]]
             else:
                 rules[number] = split[1][1:-1]
         else:
@@ -29,44 +32,28 @@ def parse_rules():
         
     return rules
 
-def find_patterns(rule_no):
-    rule = rules[rule_no]
+def make_regex(rule_no, eight_depth = 0, eleven_depth = 0):
+    if type(rule := rules[rule_no]) == str:
+        return rule
 
-    if type(rule) == str:
-        return [rule]
-    elif type(rule) == int:
-        return find_patterns(rule)
+    if rule_no == 8:
+        eight_depth += 1
+    elif rule_no == 11:
+        eleven_depth += 1
 
-    patterns = []
+    return "" if eight_depth > 5 or eleven_depth > 4 else "(" + "|".join([functools.reduce(lambda current, subrule_no:
+        current + make_regex(subrule_no, eight_depth, eleven_depth), subrule, "") for subrule in rule
+    ]) + ")"
 
-    for subrule in rule:
-        temp = []
-        
-        for subrule_no in subrule:
-            subrule_patterns = find_patterns(subrule_no)
-
-            if len(temp) == 0:
-                for subrule_pattern in subrule_patterns:
-                    temp.append("".join(subrule_pattern))
-            else:
-                temp_deep = []
-
-                for temp_item in temp:
-                    for subrule_pattern in subrule_patterns:
-                        subrule_pattern_string = "".join(subrule_pattern)
-                        temp_deep.append(temp_item + subrule_pattern_string)
-
-                temp = temp_deep
-            
-        patterns.extend(temp)
-        
-    return patterns
-
-def count_matching(messages):
-    print(len(list(filter(lambda msg: msg in patterns, messages))))
+def count_matching():
+    messages = lines[lines.index("") + 1:]
+    pattern = make_regex(0)
+    print(len(list(filter(lambda msg: re.fullmatch(pattern, msg) != None, messages))))
 
 rules = parse_rules()
-patterns = find_patterns(0)
+count_matching()
 
-messages = lines[lines.index("") + 1:]
-count_matching(messages)
+rules[8] = [[42], [42, 8]]
+rules[11] = [[42, 31], [42, 11, 31]]
+
+count_matching()
